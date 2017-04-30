b4180987c026b758371c75d15c6d514ecf3e7f6a
setoseng <setoseng@gmail.com>
2017-04-30 15:33:12 -0500

    Added a win condition

--- pygame-project/pocketmonster.py
+++ pygame-project/pocketmonster.py
@@ -22,8 +22,13 @@ class Player():
         self.player_y = 40
         self.speed_x = 0
         self.speed_y = 0
+class Goblin():
+    def __init__(self):
+        self.goblin_x = random.randint(0,480)
+        self.goblin_y = random.randint(0,480)
 player = Player()
 monster = Monster()
+goblin = Goblin()
 def setup():
     global background_image
     global player_image
@@ -39,16 +44,61 @@ def setup():
     player_image = pygame.image.load("images/hero.png")
     monster_image = pygame.image.load("images/monster.png")
 
-def redraw (collided=False, textsurface=None):
+def redraw (collided=False, textsurface=None, textsurface1=None):
     screen.fill(blue_color)
     screen.blit(background_image,[0,0])
 
     screen.blit(player_image,[player.player_x,player.player_y])
     screen.blit(monster_image,[monster.monster_x,monster.monster_y])
-    if collided and textsurface:
-        screen.blit(textsurface,(250,250))
     pygame.display.update()
     clock.tick(60)
+    if collided == True:
+        win(textsurface,textsurface1)
+
+def win(textsurface, textsurface1):
+    screen.blit(textsurface,(230,250))
+    screen.blit(textsurface1,(180,280))
+    pygame.display.update()
+    clock.tick(60)
+    while replay() == None:
+            clock.tick(60) 
+
+def replay():
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            player.player_x = player.player_x = 10
+            player.player_y = player.player_x = 20
+            monster.monster_x = monster.monster_x = random.randint(0,480)
+            monster.monster_y = monster.monster_y = random.randint(0,480)
+            return event.key
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+    return None
+
+def patrol(monster):
+    change_dir_countdown = 120
+    x_dir = 0
+    change_dir_countdown -= 1
+    if change_dir_countdown < 1:
+        change_dir_countdown = 120
+        x_dir = random.randint(0,3)
+    if x_dir == 0:
+        monster.monster_x +=2
+        if monster.monster_x> 500:
+            monster.monster_x =0
+    if x_dir == 1:       
+        monster.monster_x -=2
+        if monster.monster_x<0:
+            monster.monster_x =500
+    if x_dir == 2:
+        monster.monster_y +=2
+        if monster.monster_y>500:
+            monster.monster_y = 0
+    if x_dir == 3:
+        monster.monster_y -=2
+        if monster.monster_y<0: 
+            monster.monster_y = 500      
 
 def collided (player, monster):
     if player.player_x + 32 < monster.monster_x:
@@ -75,7 +125,9 @@ def main():
 
     pygame.font.init()
     myfont = pygame.font.SysFont('Comic Sans MS',30)
-    textsurface = myfont.render('Some Text',False,(0,0,0))
+    myfont1 = pygame.font.SysFont('Comic Sans MS',25)
+    textsurface = myfont.render('You Win!',False,(0,0,0))
+    textsurface1 =myfont1.render('Press Space to Play Again',False,(0,0,0))
 
     while not stop_game:
         for event in pygame.event.get():
@@ -156,7 +208,8 @@ def main():
         # Draw background
 
         # Game display
-        redraw(collided(player, monster), textsurface)
+        redraw(collided(player, monster), textsurface,textsurface1)
+        
 
     pygame.quit()
 if __name__ == '__main__':
